ae7480e5a4a2b1a711d13d3f50c451f21e77ed16
AlexOnGitHub <ce_sa_r4@hotmail.com>
2023-08-04 15:52:10 -0600

    Segunda fase del proyecto completada.

--- src/components/CampoTexto/index.js
+++ src/components/CampoTexto/index.js
@@ -1,9 +1,19 @@
+import { useState } from "react"
 import "./CampoTexto.css"
 
 const CampoTexto = (props) => {
+
+    const getValor = (e) => {
+        props.actualizarEquipo(e.target.value);
+    }
+
     return <div className="campo-texto">
         <label>{props.titulo}</label>
-        <input placeholder={props.placeholder}></input>
+        <input placeholder={props.placeholder} 
+        required={props.required} 
+        value={props.valor}
+        onChange={getValor}
+        ></input>
     </div>
 }
 
